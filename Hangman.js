4220173d2b71eb530c18a3546b07e0ad098ae56b
Lan Nguyen <lannguyen@Lans-Air.fios-router.home>
2021-01-28 21:29:47 -0500

    initial commit

--- Hangman.js
+++ Hangman.js
@@ -1,18 +1,49 @@
 
 class Hangman {
-
+    constructor(phrase) {
+        this.phrase = phrase;
+        this.guesses = 6;    
+        this.lettersGuessed = [];
+      }
     getDisplayMessage(){
-        return null;
+      const lowerCasePhraseAsArray = 
+      this.phrase.split('').
+      map(l => l.toLowerCase());
+      let displayMessage = '';
+      for(let i = 0; i < lowerCasePhraseAsArray.length; i++){
+        if(this.lettersGuessed.includes(lowerCasePhraseAsArray[i])){
+          displayMessage += this.phrase[i];
+        }
+        else if (lowerCasePhraseAsArray[i] == " "){
+          displayMessage += " ";
+        } else {
+          displayMessage += "*";
+        }
+    
+      }
+      return displayMessage;
+  }
+       
+    guessLetter(letter){
+        const lowerCaseLetter = letter.toLowerCase();
+        const lowerCasePhraseAsArray = 
+        this.phrase.split('').
+        map(l => l.toLowerCase());
+        if(lowerCasePhraseAsArray.includes(lowerCaseLetter)){
+          this.lettersGuessed.push(lowerCaseLetter)
+        }
+        else {
+          this.guesses--;
+        }
     }
-
-    guessLetter(letter){}
+    
 
     getRemainingGuesses(){
-        return -1;
+        return this.guesses;;
     }
 
     isMessageSolved(){
-        return false;
+        return this.getDisplayMessage().split('').every( char => char !== "*");
     }
 
 }
